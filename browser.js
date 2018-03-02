435fa08405f37ef5f48b5770acc01864e32216d9
Nathan Lunde-Berry <mr.kequc@gmail.com>
2018-03-02 22:59:14 +0100

    Fix browser event callback

--- browser.js
+++ browser.js
@@ -1,5 +1,7 @@
 const PassageClient = require('./src/client')(WebSocket);
 
-WebSocket.prototype.on = WebSocket.prototype.addEventListener;
+WebSocket.prototype.on = (name, callback) => {
+    WebSocket.prototype.addEventListener(name, ({ data }) => callback(data));
+};
 
 module.exports = PassageClient;
